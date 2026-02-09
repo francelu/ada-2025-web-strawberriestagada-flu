fa91d1c755af362a49039bc72d31fc93140ad6c3
France Lu <france.lu@epfl.ch>
2026-02-09 20:04:55 +0100

    chore: set up BackstopJS

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,56 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/flu-project_flu_strawberriestagada_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20260209-200346/flu-project_flu_strawberriestagada_Homepage_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "flu-project_flu_strawberriestagada_Homepage_0_document_0_phone.png",
+        "label": "flu strawberriestagada Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://francelu.github.io/ada-2025-web-strawberriestagada-flu/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.002729065984921564,
+          "misMatchPercentage": "0.00",
+          "analysisTime": 592
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/flu-project_flu_strawberriestagada_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20260209-200346/flu-project_flu_strawberriestagada_Homepage_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "flu-project_flu_strawberriestagada_Homepage_0_document_1_tablet.png",
+        "label": "flu strawberriestagada Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://francelu.github.io/ada-2025-web-strawberriestagada-flu/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "flu-project"
+});
