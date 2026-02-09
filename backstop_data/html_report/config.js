3175fb95027a104a23550d570cef56ab709bf5f1
France Lu <france.lu@epfl.ch>
2026-02-09 20:14:59 +0100

    fix: use right reference and test URLs

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,52 +4,56 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/flu-project_flu_strawberriestagada_Homepage_0_document_0_phone.png",
-        "test": "../bitmaps_test/20260209-200346/flu-project_flu_strawberriestagada_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20260209-201357/flu-project_flu_strawberriestagada_Homepage_0_document_0_phone.png",
         "selector": "document",
         "fileName": "flu-project_flu_strawberriestagada_Homepage_0_document_0_phone.png",
         "label": "flu strawberriestagada Homepage",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
-        "url": "https://francelu.github.io/ada-2025-web-strawberriestagada-flu/",
-        "referenceUrl": "",
+        "url": "http://localhost:4000",
+        "referenceUrl": "https://francelu.github.io/ada-2025-web-strawberriestagada-flu/",
         "expect": 0,
         "viewportLabel": "phone",
         "diff": {
-          "isSameDimensions": true,
+          "isSameDimensions": false,
           "dimensionDifference": {
-            "width": 0,
-            "height": 0
+            "width": -619,
+            "height": -58469
           },
-          "rawMisMatchPercentage": 0.002729065984921564,
-          "misMatchPercentage": "0.00",
-          "analysisTime": 592
-        }
+          "rawMisMatchPercentage": 0.9573923170621602,
+          "misMatchPercentage": "0.96",
+          "analysisTime": 537
+        },
+        "diffImage": "../bitmaps_test/20260209-201357/failed_diff_flu-project_flu_strawberriestagada_Homepage_0_document_0_phone.png"
       },
-      "status": "pass"
+      "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/flu-project_flu_strawberriestagada_Homepage_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20260209-200346/flu-project_flu_strawberriestagada_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20260209-201357/flu-project_flu_strawberriestagada_Homepage_0_document_1_tablet.png",
         "selector": "document",
         "fileName": "flu-project_flu_strawberriestagada_Homepage_0_document_1_tablet.png",
         "label": "flu strawberriestagada Homepage",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
-        "url": "https://francelu.github.io/ada-2025-web-strawberriestagada-flu/",
-        "referenceUrl": "",
+        "url": "http://localhost:4000",
+        "referenceUrl": "https://francelu.github.io/ada-2025-web-strawberriestagada-flu/",
         "expect": 0,
         "viewportLabel": "tablet",
         "diff": {
-          "isSameDimensions": true,
+          "isSameDimensions": false,
           "dimensionDifference": {
-            "width": 0,
-            "height": 0
+            "width": -27,
+            "height": -36311
           },
-          "misMatchPercentage": "0.00"
-        }
+          "rawMisMatchPercentage": 3.567789966377465,
+          "misMatchPercentage": "3.57",
+          "analysisTime": 413
+        },
+        "diffImage": "../bitmaps_test/20260209-201357/failed_diff_flu-project_flu_strawberriestagada_Homepage_0_document_1_tablet.png"
       },
-      "status": "pass"
+      "status": "fail"
     }
   ],
   "id": "flu-project"
